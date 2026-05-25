0472ddbd89030252d34f3f44f18f302a16fe483d
RAMBO-LC <rambo4code@gmail.com>
2026-05-25 17:37:36 +0530

    made a bulb

--- DOM/bulb on/main.js
+++ DOM/bulb on/main.js
@@ -1,3 +1,21 @@
 var a = document.querySelector("#bulb")
 var b = document.querySelector("body")
 
+a.addEventListener("click", function () {
+    if (a.innerHTML === "ON") {
+        a.style.backgroundColor = "grey"
+        b.style.backgroundColor = "lightgray"
+
+        a.innerHTML = "OFF";
+        console.log("LIGHT band")
+    }
+    else {
+        a.style.backgroundColor = "yellow"
+        b.style.backgroundColor = "lightyellow"
+
+        a.innerHTML = "ON";
+        console.log("LIGHT shuru")
+
+    }
+
+})
